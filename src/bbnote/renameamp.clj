1d8ecb9d00c00f246331ec314c253ff40343d1ce
Rdk0 <radeklaufer@gmail.com>
2025-09-16 21:51:20 -0400

    renameamp fix

--- src/bbnote/renameamp.clj
+++ src/bbnote/renameamp.clj
@@ -19,25 +19,26 @@
   ;;        (u/rename-pdf (.getName file) (remove-amp-in-string  (.getName file))))))
 
   (defn rename-pdfs-in-directory
-    "for every pdf or md in the directory remove &amp"
+    "for every pdf or md in the directory rename all files to replace &amp with underscore"
     [dir-path]
     (let [direc (io/file dir-path)]
-      (->> (file-seq direc)
-           (filter (fn [file]
-                     (and (.isFile file)
-                          (clojure.string/includes? (.getName file) "&") ; use clojure.string/includes?
-                          (or (clojure.string/ends-with? (clojure.string/lower-case (.getName file)) ".pdf")
-                              (clojure.string/ends-with? (clojure.string/lower-case (.getName file)) ".md")))))
-           (run! (fn [file]
-                   (let [new-name (remove-amp-in-string (.getName file))]
-                     (println (.getName file) new-name)
-                     (u/rename-pdf (.getName file) new-name)))))))
+      ( ->> (.listFiles direc)
+       (filter (fn [file]
+                 (let [file-name (.getName file)]
+                   (and (clojure.string/includes? file-name "&") 
+                        (or (clojure.string/ends-with? (clojure.string/lower-case file-name) ".pdf")
+                            (clojure.string/ends-with? (clojure.string/lower-case file-name) ".md"))))))
+       (run! (fn [file]
+               (let [file-path (.getPath file)
+                     new-name-path (remove-amp-in-string file-path)]
+                 (println file-path  new-name-path)
+                 (u/rename-pdf file-path new-name-path)))))))
   
 
 (defn rename-pdfs-in-directories 
   "remove &amp from pdf filenames in main and md directories"
   [opts]
   (let [dir-path (:path opts)]
-    (rename-pdfs-in-directory dir-path)))
-   ; (rename-pdfs-in-directory (str dir-path "/md"))))
+    (rename-pdfs-in-directory dir-path)
+    (rename-pdfs-in-directory (str dir-path "/md")))
    
